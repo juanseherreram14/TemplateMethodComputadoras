48a483dd0d73eb1aa0e2ce0811bcd7212a0e98c7
juans <juanseherreram14@gmail.com>
2023-01-25 16:49:36 -0500

    Add project files.

---
+++ TemplateMethodComputadoras/Ordenador.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethodComputadoras
+{
+   abstract class Ordenador
+{
+    public void Ensamblar()
+    {
+            Console.Write("Instalando tarjeta madre...");
+            Thread.Sleep(2000);
+            InstalarTarjetaMadre();
+
+            Console.Write("Instalando procesador...");
+            Thread.Sleep(2000);
+            InstalarProcesador();
+
+            Console.Write("Instalando memoria RAM...");
+            Thread.Sleep(2000);
+            InstalarMemoriaRAM();
+
+            Console.Write("Instalando componentes adicionales...");
+            Thread.Sleep(2000);
+            InstalarComponentesAdicionales();
+        }
+    public abstract void InstalarTarjetaMadre();
+    public abstract void InstalarProcesador();
+    public abstract void InstalarMemoriaRAM();
+    public abstract void InstalarComponentesAdicionales();
+}
+
+class OrdenadorGaming : Ordenador
+{
+    public override void InstalarTarjetaMadre()
+    {
+        Console.WriteLine("Tarjeta madre para juegos de alto rendimiento instalada");
+    }
+    public override void InstalarProcesador()
+    {
+        Console.WriteLine("Procesador de alta velocidad instalado");
+    }
+    public override void InstalarMemoriaRAM()
+    {
+        Console.WriteLine("Memoria RAM DDR4 instalada");
+    }
+    public override void InstalarComponentesAdicionales()
+    {
+        Console.WriteLine("Tarjeta gráfica de alto rendimiento instalada");
+    }
+}
+
+class OrdenadorOficina : Ordenador
+{
+    public override void InstalarTarjetaMadre()
+    {
+        Console.WriteLine("Tarjeta madre económica instalada");
+    }
+    public override void InstalarProcesador()
+    {
+        Console.WriteLine("Procesador de velocidad media instalado");
+    }
+    public override void InstalarMemoriaRAM()
+    {
+    Console.WriteLine("Memoria RAM DDR3 instalada");
+    }
+    public override void InstalarComponentesAdicionales()
+    {
+        Console.WriteLine("No se instalaron componentes adicionales");
+    }
+}
+
+}
---
+++ TemplateMethodComputadoras/Program.cs
@@ -0,0 +1,34 @@
+﻿using TemplateMethodComputadoras;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("Seleccione el tipo de ordenador que desea ensamblar:");
+            Console.WriteLine("1. Computadora Gamer");
+            Console.WriteLine("2. Computadora de Oficina");
+            Console.WriteLine("3. Salir");
+
+            int opcion = Convert.ToInt32(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    OrdenadorGaming ordenadorGaming = new OrdenadorGaming();
+                    ordenadorGaming.Ensamblar();
+                    break;
+                case 2:
+                    OrdenadorOficina ordenadorOficina = new OrdenadorOficina();
+                    ordenadorOficina.Ensamblar();
+                    break;
+                case 3:
+                    return;
+                default:
+                    Console.WriteLine("Opción no válida");
+                    break;
+            }
+        }
+    }
+}
